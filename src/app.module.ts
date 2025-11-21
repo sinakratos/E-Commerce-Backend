32d6f7352d20db979201823ba95d23e9567167b5
sinakratos <sinakhoshzaban2@gmail.com>
2025-11-22 00:17:08 +0330

    Fixed imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { DBModule } from './config/db.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DBModule, UsersModule, AuthModule],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DBModule,
+    UsersModule,
+    AuthModule,
+  ],
   providers: [],
 })
 export class AppModule {}

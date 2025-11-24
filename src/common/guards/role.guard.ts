3a5d57eb8a905c40ede43682750527cec8ce8394
sinakratos <sinakhoshzaban2@gmail.com>
2025-11-24 18:39:13 +0330

    Added ROLES_KEY

--- src/common/guards/role.guard.ts
+++ src/common/guards/role.guard.ts
@@ -6,6 +6,8 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
 import { Role } from 'src/common/enums/Role.enum';
 
 @Injectable()
@@ -13,14 +15,13 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', [
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
     if (!requiredRoles || requiredRoles.length === 0) {
       return true;
     }
-
     const request = context.switchToHttp().getRequest();
     const user = request.user;
 
